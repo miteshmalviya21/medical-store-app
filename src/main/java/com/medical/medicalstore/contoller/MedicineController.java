be57c9149095037176efab95ecc6cc49c5a5c6c1
unknown <mitesh21malviya@gmail.com>
2026-04-07 17:03:04 +0530

    Added controller Medicine file

---
+++ src/main/java/com/medical/medicalstore/contoller/MedicineController.java
@@ -0,0 +1,10 @@
+package com.medical.medicalstore.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(name = "/medicines")
+public class MedicineController {
+
+}
