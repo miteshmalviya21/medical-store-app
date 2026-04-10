c3e85875db5d479988008a96650fd09cb940dfad
unknown <mitesh21malviya@gmail.com>
2026-04-10 22:22:54 +0530

    Added Controller java files

--- src/main/java/com/medical/medicalstore/contoller/MedicineController.java
+++ src/main/java/com/medical/medicalstore/contoller/MedicineController.java
@@ -3,8 +3,16 @@ package com.medical.medicalstore.contoller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.medical.medicalstore.service.MedicineService;
+
 @Controller
 @RequestMapping(name = "/medicines")
 public class MedicineController {
 
+	private final MedicineService service;
+	
+	public MedicineController(MedicineService service) {
+		this.service = service;
+	}
+	
 }
