58c076e9b84c47d2cff85fa8c141a3bc20a672ef
unknown <mitesh21malviya@gmail.com>
2026-04-12 22:11:52 +0530

    Updating MedicineController.java file added get and post mapping method

--- src/main/java/com/medical/medicalstore/contoller/MedicineController.java
+++ src/main/java/com/medical/medicalstore/contoller/MedicineController.java
@@ -1,8 +1,14 @@
 package com.medical.medicalstore.contoller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.medical.medicalstore.entity.Medicine;
 import com.medical.medicalstore.service.MedicineService;
 
 @Controller
@@ -13,6 +19,16 @@ public class MedicineController {
 	
 	public MedicineController(MedicineService service) {
 		this.service = service;
+	}
+	
+	@PostMapping
+	public Medicine add(@RequestBody Medicine medicine) {
+		return service.save(medicine);
+	}
+	
+	@GetMapping
+	public List<Medicine> getAll(){
+		return service.getAll();
 	}
 	
 }
